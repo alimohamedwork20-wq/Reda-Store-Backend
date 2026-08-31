3296f052b22627eff6c3c54d5391c25e55537da1
ali_mohamed <ali.mohamed.work20@gmail.com>
2026-08-31 09:01:27 +0300

    Remove client supplied user id from report DTO

--- Reda/Dtos/ReportDto.cs
+++ Reda/Dtos/ReportDto.cs
@@ -5,7 +5,6 @@
         public string Category { get; set; }
         public string Subject { get; set; }
         public string Description { get; set; }
-        public int UserId { get; set; }
         public IFormFile? Screenshot { get; set; }
     }
 }

a00bf79880381bcd83e5087837666d39b809d945
ali_mohamed <ali.mohamed.work20@gmail.com>
2026-08-31 09:26:15 +0300

    Pass authenticated user ID separately from address DTOs

--- Reda/Interfaces/IAccountService.cs
+++ Reda/Interfaces/IAccountService.cs
@@ -7,14 +7,14 @@ namespace Reda.Interfaces
     {
         Task<string> ChangePasswordAsync(int id, string oldPassword, string newPassword);
         Task<string> ChangeNameAsync(int id, string name);
-        Task<string> ChangeEmailAsync(int id, string email,string code);
+        Task<string> ChangeEmailAsync(int id, string email, string code);
         Task<string> AddPhoneAsync(int id, string email, string phone, string code);
         Task<string> DeleteAccountAsync(int id);
         Task<string> AddProfileImage(IFormFile image, int userId);
         Task<string> RemoveProfileImage(int userId);
         Task<List<Addresses>> GetUserAddressesAsync(int userId);
-        Task<string> AddAddressAsync(AddAddressDto addresses);
-        Task<string> DeleteAddressAsync(ChangeAndDeleteAddressDto addresses);
-        Task<string> SetDefaultAddressAsync(ChangeAndDeleteAddressDto addresses);
+        Task<string> AddAddressAsync(AddAddressDto address, int userId);
+        Task<string> DeleteAddressAsync(int addressId, int userId);
+        Task<string> SetDefaultAddressAsync(int addressId, int userId);
     }
 }

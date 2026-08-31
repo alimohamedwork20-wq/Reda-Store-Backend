110b64284278e2cd3d16f4531c1d5c9f4c750b56
ali_mohamed <ali.mohamed.work20@gmail.com>
2026-08-31 09:00:33 +0300

    Update auth service contract for claims-based user identity

--- Reda/Interfaces/IAuthService.cs
+++ Reda/Interfaces/IAuthService.cs
@@ -10,8 +10,8 @@ namespace Reda.Interfaces
         Task<string> SendCodeToEmailAsync(string email);
         Task<string> CheckOtpToChangePasswordAsync(CheckOtpToChangePasswordDto model);
         Task<string> ResetPasswordAsync(ResetPasswordDto model);
-        Task<string> TurnOnTwoFactorAsync(UserIdDto model);
-        Task<string> TurnOffTwoFactorAsync(UserIdDto model);
-        Task<User> GetUserById(UserIdDto model);
+        Task<string> TurnOnTwoFactorAsync(int userId);
+        Task<string> TurnOffTwoFactorAsync(int userId);
+        Task<User> GetCurrentUserAsync(int userId);
     }
 }

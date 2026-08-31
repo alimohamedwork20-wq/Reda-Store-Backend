bfaff4ab7dc246a0c4b9577cbdf5b0392e59934b
ali_mohamed <ali.mohamed.work20@gmail.com>
2026-08-31 19:24:25 +0300

    Return cart items with persisted quantities

--- Reda/Interfaces/IProductService.cs
+++ Reda/Interfaces/IProductService.cs
@@ -1,3 +1,4 @@
+using Reda.Dtos;
 using Reda.Entities;
 
 namespace Reda.Interfaces
@@ -6,13 +7,13 @@ namespace Reda.Interfaces
     {
         Task<List<Product>> GetProductsByCategoryAsync(string slug);
         Task<Product> GetProductByIdAsync(int id);
-        Task<string> AddProductToCart(int productId, int idUser);
-        Task<List<Product>> GetProductsInCart(int idUser);
-        Task<string> UpdateCartQuantity(int idUser, int idProduct, int quantity);
-        Task<string> DeleteProductInCart(int idUser, int idProduct);
-        Task<string> DeleteAllProductsInCart(int idUser);
-        Task<string> AddProductToFavorite(int idUser, int idProduct);
-        Task<string> DeleteProductFromFavorite(int idUser, int idProduct);
+        Task<string> AddProductToCart(int productId, int userId);
+        Task<List<CartProductDto>> GetProductsInCart(int userId);
+        Task<string> UpdateCartQuantity(int userId, int productId, int quantity);
+        Task<string> DeleteProductInCart(int userId, int productId);
+        Task<string> DeleteAllProductsInCart(int userId);
+        Task<string> AddProductToFavorite(int userId, int productId);
+        Task<string> DeleteProductFromFavorite(int userId, int productId);
         Task<List<Product>> GetProductFromFavorite(int userId);
         Task<List<Product>> SearchProduct(string name);
     }
